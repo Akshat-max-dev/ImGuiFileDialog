7b5804763dcf5594320dfebe1868d9defe71975e
Aero Akshat <itgumanmal@gmail.com>
2021-04-15 14:20:19 +0530

    mistake

--- ImGuiFileDialogConfig.h
+++ ImGuiFileDialogConfig.h
@@ -5,17 +5,17 @@
 //#define MAX_FILE_DIALOG_NAME_BUFFER 1024
 //#define MAX_PATH_BUFFER_SIZE 1024
 
-#define USE_EXPLORATION_BY_KEYS
+//#define USE_EXPLORATION_BY_KEYS
 // this mapping by default is for GLFW but you can use another
-#include <GLFW/glfw3.h> 
+//#include <GLFW/glfw3.h> 
 // Up key for explore to the top
-#define IGFD_KEY_UP GLFW_KEY_UP
+//#define IGFD_KEY_UP GLFW_KEY_UP
 // Down key for explore to the bottom
-#define IGFD_KEY_DOWN GLFW_KEY_DOWN
+//#define IGFD_KEY_DOWN GLFW_KEY_DOWN
 // Enter key for open directory
-#define IGFD_KEY_ENTER GLFW_KEY_ENTER
+//#define IGFD_KEY_ENTER GLFW_KEY_ENTER
 // BackSpace for comming back to the last directory
-#define IGFD_KEY_BACKSPACE GLFW_KEY_BACKSPACE
+//#define IGFD_KEY_BACKSPACE GLFW_KEY_BACKSPACE
 
 // widget
 // filter combobox width
